0f44e8c5f0e4ea1ec97063db7c0c96ba1182e74b
99004385-NIlesh <84907773+99004385-NIlesh@users.noreply.github.com>
2021-06-07 00:05:37 +0530

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -4,4 +4,5 @@ This module will print the names of all the sheets in the Excel File
 import active_excel
 WORKBOOK = active_excel.WORKBOOK
 def print_sheets():
+    """ This will return the list of all the sheet names """
     return WORKBOOK.get_sheet_names()
